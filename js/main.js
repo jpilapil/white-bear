a6ad4cefebb99a18e322df162d516ff22dd57b8d
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-05-07 16:58:27 -0700

    add jquery and edit login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// disables sign-up-card2 on page load
 
+$(document).ready(function () {
+   $("#sign-up-card2").toggle();
+});
+
+$("#sign-up-button").click(function () {
+   $("#sign-up-button").toggle();
+   $("#sign-up-card2").toggle();
+});
