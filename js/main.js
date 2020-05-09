a8573f01a496d6ef76826d2d4035fbc3e2e95902
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-05-08 23:05:19 -0700

    add review and success/error ques

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-// disables sign-up-card2 on page load
+// disables sign-up-card2 on page load for landing page
 
 $(document).ready(function () {
    $("#sign-up-card2").toggle();
 });
 
+// toggles second sign up
+
 $("#sign-up-button").click(function () {
    $("#sign-up-button").toggle();
    $("#sign-up-card2").toggle();
+});
+
+// toggles card save success when clicked
+
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+// toggles error cue when clicked
+$("#delete-imagery").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
 });
