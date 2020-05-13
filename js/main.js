996c47d000439674b0b56f41c3c5bccf46925e3a
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-05-13 11:19:37 -0700

    add email and password requirement jquery

--- js/main.js
+++ js/main.js
@@ -28,3 +28,41 @@ $("#show-delete").click(function () {
 $("#delete-card").click(function () {
    $("#delete-success").toggleClass("d-flex d-none");
 });
+
+// LANDING PAGE
+
+// email for first card
+$("#lets-go").click(function () {
+   var checkEmail = $("#exampleInputEmail1").val().length;
+   if (checkEmail == 0) {
+      $(".email-error1").toggleClass("d-none");
+      return false;
+   }
+});
+
+// email for second card
+$("#log-in").click(function () {
+   var checkEmail = $("#exampleInputEmail2").val().length;
+   if (checkEmail == 0) {
+      $(".email-error2").toggleClass("d-none");
+      return false;
+   }
+});
+
+// password for first card
+$("#lets-go").click(function () {
+   var checkPassword = $("#exampleInputPassword1").val().length;
+   if (checkPassword < 8) {
+      $(".password-error1").toggleClass("d-none");
+      return false;
+   }
+});
+
+// password for second card
+$("#log-in").click(function () {
+   var checkPassword = $("#exampleInputPassword2").val().length;
+   if (checkPassword < 8) {
+      $(".password-error2").toggleClass("d-none");
+      return false;
+   }
+});
